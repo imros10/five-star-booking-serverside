4da804d59e191f49bde572d2b4c8c8f47aa03816
imros10 <imros.arefen@gmail.com>
2021-10-31 20:34:26 +0600

    Working

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const { MongoClient } = require('mongodb');
 const cors = require('cors');
+const ObjectId = require('mongodb').ObjectId;
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 
@@ -17,11 +18,47 @@ async function run(){
         await client.connect();
         const database = client.db('hotel');
         const roomsDB = database.collection('rooms');
+        const ordersDB = database.collection('orders');
+        // load data 
         app.get('/rooms',async(req,res)=>{
             const quary = roomsDB.find({});
             const rooms = await quary.toArray();
             res.json(rooms);
         })
+        // post order in database 
+        app.post('/orders',async(req,res)=>{
+            const quary =  req.body;
+            const result = await ordersDB.insertOne(quary)
+            res.json(result)
+          
+        }) 
+        // get order data 
+        app.get('/orders',async(req,res)=>{
+            const quary = ordersDB.find({});
+            const result = await quary.toArray();
+            res.json(result)
+        })
+        // delete an item 
+        app.delete('/orders/:id',async(req,res)=>{
+            const id = req.params.id;
+            console.log(id)
+            const quary = {_id: ObjectId(id)};
+            const result = await ordersDB.deleteOne(quary);
+            if (result.deletedCount === 1) {
+                console.log("Successfully deleted one document.");
+              } else {
+                console.log("No documents matched the query. Deleted 0 documents.");
+              }
+            res.json(result)
+        })
+
+        // post new service 
+        app.post('/rooms',async(req,res)=>{
+            const quary = req.body;
+            const result = await roomsDB.insertOne(quary);
+            res.json(result)
+        })
+
     }finally{
 
     }
